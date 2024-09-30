b9fea6f44e3f4e123f5438fde3a366b1fcb00fbd
liyuhao <liyuhaoid@sina.com>
2024-09-30 16:40:56 +0800

    准备第一版上线

--- chooser/build.gradle.kts
+++ chooser/build.gradle.kts
@@ -1,6 +1,12 @@
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.Properties
+import net.thebugmc.gradle.sonatypepublisher.PublishingType.AUTOMATIC
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.3"
 }
 
 android {
@@ -36,4 +42,83 @@ dependencies {
     //api(project(":selector"))
     api("io.github.liyuhaolol:PictureSelector:v3.11.3")
     //implementation("com.google.android.material:material:1.12.0")
+}
+
+var signingKeyId = ""//签名的密钥后8位
+var signingPassword = ""//签名设置的密码
+var secretKeyRingFile = ""//生成的secring.gpg文件目录
+var ossrhUsername = ""//sonatype用户名
+var ossrhPassword = "" //sonatype密码
+
+val localProperties = project.rootProject.file("local.properties")
+
+if (localProperties.exists()) {
+    println("Found secret props file, loading props")
+    val properties = Properties()
+
+    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
+        properties.load(reader)
+    }
+    signingKeyId = properties.getProperty("signingKeyId")
+    signingPassword = properties.getProperty("signingPassword")
+    secretKeyRingFile = properties.getProperty("secretKeyRingFile")
+    ossrhUsername = properties.getProperty("ossrhUsername")
+    ossrhPassword = properties.getProperty("ossrhPassword")
+
+} else {
+    println("No props file, loading env vars")
+}
+
+
+centralPortal {
+    username = ossrhUsername
+    password = ossrhPassword
+    name = "PictureChooser"
+    group = "io.github.liyuhaolol"
+    version = "1.0.0"
+    pom {
+        //packaging = "aar"
+        name = "PictureChooser"
+        description = "Android PictureSelector Utils"
+        url = "https://github.com/liyuhaolol/PictureSelector"
+        licenses {
+            license {
+                name = "Apache License"
+                url = "https://github.com/liyuhaolol/PictureSelector/blob/master/LICENSE"
+            }
+        }
+        developers {
+            developer {
+                id = "liyuhao"
+                name = "liyuhao"
+                email = "liyuhaoid@sina.com"
+            }
+        }
+        scm {
+            connection = "scm:git@github.com/liyuhaolol/PictureSelector.git"
+            developerConnection = "scm:git@github.com/liyuhaolol/PictureSelector.git"
+            url = "https://github.com/liyuhaolol/PictureSelector"
+        }
+
+    }
+    publishingType = AUTOMATIC
+    javadocJarTask = tasks.create<Jar>("javadocEmptyJar") {
+        archiveClassifier = "javadoc"
+    }
+
+}
+
+
+gradle.taskGraph.whenReady {
+    if (allTasks.any { it is Sign }) {
+        allprojects {
+            extra["signing.keyId"] = signingKeyId
+            extra["signing.secretKeyRingFile"] = secretKeyRingFile
+            extra["signing.password"] = signingPassword
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
