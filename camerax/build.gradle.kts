9d8cfb2677f06dfa5d85774cf0515d55d6b437a8
liyuhao <liyuhaoid@sina.com>
2024-10-12 09:36:02 +0800

    camerax的UI适配Android15

---
+++ camerax/build.gradle.kts
@@ -0,0 +1,126 @@
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.Properties
+import net.thebugmc.gradle.sonatypepublisher.PublishingType.AUTOMATIC
+
+plugins {
+    id("com.android.library")
+    id("net.thebugmc.gradle.sonatype-central-portal-publisher") version "1.2.3"
+}
+
+android {
+    namespace = "com.luck.lib.camerax"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    api ("androidx.camera:camera-core:1.2.1")
+    api ("androidx.camera:camera-camera2:1.2.1")
+    implementation ("androidx.camera:camera-view:1.2.1")
+    implementation ("androidx.camera:camera-lifecycle:1.2.1")
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+    implementation ("androidx.transition:transition:1.5.1")
+    implementation ("androidx.concurrent:concurrent-futures:1.2.0")
+}
+
+var signingKeyId = ""//签名的密钥后8位
+var signingPassword = ""//签名设置的密码
+var secretKeyRingFile = ""//生成的secring.gpg文件目录
+var ossrhUsername = ""//sonatype用户名
+var ossrhPassword = "" //sonatype密码
+
+val localProperties = project.rootProject.file("local.properties")
+
+if (localProperties.exists()) {
+    println("Found secret props file, loading props")
+    val properties = Properties()
+
+    InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
+        properties.load(reader)
+    }
+    signingKeyId = properties.getProperty("signingKeyId")
+    signingPassword = properties.getProperty("signingPassword")
+    secretKeyRingFile = properties.getProperty("secretKeyRingFile")
+    ossrhUsername = properties.getProperty("ossrhUsername")
+    ossrhPassword = properties.getProperty("ossrhPassword")
+
+} else {
+    println("No props file, loading env vars")
+}
+
+
+centralPortal {
+    username = ossrhUsername
+    password = ossrhPassword
+    name = "camerax"
+    group = "io.github.liyuhaolol"
+    version = "v3.11.3"
+    pom {
+        //packaging = "aar"
+        name = "camerax"
+        description = "Android PictureSelector Utils"
+        url = "https://github.com/liyuhaolol/PictureSelector"
+        licenses {
+            license {
+                name = "Apache License"
+                url = "https://github.com/liyuhaolol/PictureSelector/blob/master/LICENSE"
+            }
+        }
+        developers {
+            developer {
+                id = "liyuhao"
+                name = "liyuhao"
+                email = "liyuhaoid@sina.com"
+            }
+        }
+        scm {
+            connection = "scm:git@github.com/liyuhaolol/PictureSelector.git"
+            developerConnection = "scm:git@github.com/liyuhaolol/PictureSelector.git"
+            url = "https://github.com/liyuhaolol/PictureSelector"
+        }
+
+    }
+    publishingType = AUTOMATIC
+    javadocJarTask = tasks.create<Jar>("javadocEmptyJar") {
+        archiveClassifier = "javadoc"
+    }
+
+}
+
+
+gradle.taskGraph.whenReady {
+    if (allTasks.any { it is Sign }) {
+        allprojects {
+            extra["signing.keyId"] = signingKeyId
+            extra["signing.secretKeyRingFile"] = secretKeyRingFile
+            extra["signing.password"] = signingPassword
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
+}
