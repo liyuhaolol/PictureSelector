c9a691f989533f68db39960fc81ada6cbcb50327
liyuhao <liyuhaoid@sina.com>
2025-01-28 22:45:35 +0800

    更新selector v3.11.5

--- selector/build.gradle.kts
+++ selector/build.gradle.kts
@@ -79,7 +79,7 @@ centralPortal {
     password = ossrhPassword
     name = "PictureSelector"
     group = "io.github.liyuhaolol"
-    version = "v3.11.4"
+    version = "v3.11.5"
     pom {
         //packaging = "aar"
         name = "PictureSelector"
